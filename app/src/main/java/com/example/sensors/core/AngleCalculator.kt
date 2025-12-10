0759b490ec31f33c39086ad59958eafd28e30e2b
Alexby03 <alexanderby03@gmail.com>
2025-12-10 16:02:31 +0100

    Changed formula for linear acceleration and tweaked alpha values.

--- app/src/main/java/com/example/sensors/core/AngleCalculator.kt
+++ app/src/main/java/com/example/sensors/core/AngleCalculator.kt
@@ -2,14 +2,16 @@ package com.example.sensors.core
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlin.math.acos
+import kotlin.math.sqrt
 
 class AngleCalculator {
     private var lastFilteredAcceleratorAngle = 0f
     private var currentFusionAngle = 0f
     private var lastTimestamp: Long = 0
 
-    private val aEWMA = 0.98f
-    private val aFusion = 0.02f
+    private val aEWMA = 0.05f
+    private val aFusion = 0.75f
 
     fun process(rawEvents: Flow<ReadSensorEvent>): Flow<MeasuredResult> {
         reset()
@@ -22,12 +24,13 @@ class AngleCalculator {
             }
             lastTimestamp = event.timestamp
 
+            val ax = event.accelerometerValues[0]
             val ay = event.accelerometerValues[1]
             val az = event.accelerometerValues[2]
 
             val gyroX = event.gyroscopeValues[0]
 
-            val angle1 = accelerationAngle(ay, az)
+            val angle1 = accelerationAngle(ax, ay, az)
             val angle2 = fusionAngle(angle1, gyroX, dt)
 
             MeasuredResult(
@@ -39,9 +42,16 @@ class AngleCalculator {
     }
 
     // algorithm #1
-    private fun accelerationAngle(ay: Float, az: Float): Float {
-        val angle = Math.toDegrees(kotlin.math.atan2(az.toDouble(), ay.toDouble())).toFloat()
-        val filteredAngle = aEWMA * angle + (1 - aEWMA) * lastFilteredAcceleratorAngle
+    private fun accelerationAngle(ax: Float, ay: Float, az: Float): Float {
+        val norm = sqrt(ax*ax + ay*ay + az*az)
+        val cosTilt = ay / norm
+        val angle = Math.toDegrees(acos(cosTilt).toDouble())
+        if (lastFilteredAcceleratorAngle == 0f && angle != 0.0) {
+            lastFilteredAcceleratorAngle = angle.toFloat()
+            return lastFilteredAcceleratorAngle
+        }
+
+        val filteredAngle = (aEWMA * angle + (1 - aEWMA) * lastFilteredAcceleratorAngle).toFloat()
         lastFilteredAcceleratorAngle = filteredAngle
         return filteredAngle
     }
